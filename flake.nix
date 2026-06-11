e947f5d29e8f169a2d2116275ca52b19c3985325
ProRagerXx <electrorgr973@gmail.com>
2026-06-11 19:10:14 -0400

    initial commit

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "My NixOS";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
