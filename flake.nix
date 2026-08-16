50bd0df37b8bb7dc55c3c1bdf9b1d9cfdbcb17f7
ProRagerXx <electrorgr973@gmail.com>
2026-08-16 12:40:25 -0400

    update & install nix-gaming

--- flake.nix
+++ flake.nix
@@ -1,10 +1,15 @@
 {
   description = "My NixOS";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    nix-gaming.url = "github:fufexan/nix-gaming";
+  };
 
   outputs = { self, nixpkgs, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      #specialArgs = {inherit inputs;};
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
